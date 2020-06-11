ef6567a30a82968b99d0a5964c8b42baa2b51ed4
Emily Horne <emilyhorne@Emilys-MacBook-Pro.local>
2020-06-11 14:39:39 -0600

    task 7 is finished

--- index.js
+++ index.js
@@ -87,9 +87,10 @@ and should return a string in the format `{name} gave the restaurant a {rating},
  * it will return `Daniela gave the restaurant a 5 star review and their feedback was: Beautiful atmosphere and wonderful vegan options!`
 */
 function getReviewByIndex(reviews, index) {
-    /* code here */
+   
+  return `${reviews[index].name} gave the restaurant a ${reviews[index].rating} star review and their feedback was: ${reviews[index].feedback}`;
   }
-  
+  console.log (getReviewByIndex(reviews, 1));
 
 /* Task 8: Write a function to get information about the most recent review called `getLastReview`
 
